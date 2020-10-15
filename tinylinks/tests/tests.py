35b5786b8110a762beba2cde743dc240f739f073
Gerges Fikry <45514405+lavindiuss@users.noreply.github.com>
2020-10-15 14:17:16 +0200

    fix-TinylinkViewSet list & create (#9)
    * fix-TinylinkViewSet list & create
    
    * fix-TinylinkViewSet , add unit test
    
    * fix-TinylinkRestViewTest, add unit tet
    
    * fix-TinylinkRestViewTest, add unit test
    
    * clear  anonymous user from tests
    
    * Update changelog
    
    * Add readonly permission to TinylinkViewSet
    
    * remove-- TokenAuthentication from rest views
    
    * fix--naming conventions
    
    * return absolute_uri from TinylinkViewSet creating endpoint

--- tinylinks/tests/tests.py
+++ tinylinks/tests/tests.py
@@ -1,3 +1,5 @@
+import base64
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
@@ -6,37 +8,49 @@ from rest_framework.test import APITestCase
 
 from ..models import Tinylink, TinylinkLog
 from .factories import TinylinkFactory, UserFactory, TinyLogFactory
+
 User = get_user_model()
 
 
 class TinyLinkTest(APITestCase):
-
     def setUp(self):
         super(TinyLinkTest, self).setUp()
-        self.tiny_link_list_url = reverse('tinylink_list')
-        self.tiny_link_create_url = reverse('tinylink_create')
-        self.user = User.objects.create_superuser(username='gunjan_test',
-                                                  email='gunjan_test@kuwaitnet.com',
-                                                  password=make_password('test1234'))
-        self.tiny_link = Tinylink.objects.create(user=self.user,
-                                                 long_url="http://www.example.com/thisisalongURL",
-                                                 short_url="vB7f5b")
-        self.tiny_log = TinylinkLog.objects.create(tinylink=self.tiny_link,
-                                                   referrer='http://www.example.com/thisisalongURL',
-                                                   user_agent='Gunjan Modi',
-                                                   cookie='test:test',
-                                                   remote_ip='127.0.0.1',
-                                                   tracked=True)
-        self.tiny_link_update_long_url = reverse('tinylink_update', kwargs={'pk': self.tiny_link.id,
-                                                                            'mode': 'change-long'})
-        self.tiny_link_update_short_url = reverse('tinylink_update', kwargs={'pk': self.tiny_link.id,
-                                                                            'mode': 'change-short'})
+        self.tiny_link_list_url = reverse("tinylink_list")
+        self.tiny_link_create_url = reverse("tinylink_create")
+        self.user = User.objects.create_superuser(
+            username="gunjan_test",
+            email="gunjan_test@kuwaitnet.com",
+            password=make_password("test1234"),
+        )
+        self.tiny_link = Tinylink.objects.create(
+            user=self.user,
+            long_url="http://www.example.com/thisisalongURL",
+            short_url="vB7f5b",
+        )
+        self.tiny_log = TinylinkLog.objects.create(
+            tinylink=self.tiny_link,
+            referrer="http://www.example.com/thisisalongURL",
+            user_agent="Gunjan Modi",
+            cookie="test:test",
+            remote_ip="127.0.0.1",
+            tracked=True,
+        )
+        self.tiny_link_update_long_url = reverse(
+            "tinylink_update", kwargs={"pk": self.tiny_link.id, "mode": "change-long"}
+        )
+        self.tiny_link_update_short_url = reverse(
+            "tinylink_update", kwargs={"pk": self.tiny_link.id, "mode": "change-short"}
+        )
 
-        self.tiny_link_statistics = reverse('tinylink_statistics')
-        self.tiny_link_db_stats = reverse('api_db_stats')
-        self.tiny_link_api_stats = reverse('api_db_stats')
-        self.tiny_link_url_stats = reverse('api_url_stats', kwargs={'short_url': self.tiny_link.short_url})
-        self.tiny_link_expand_stats = reverse('api_tinylink_expand', kwargs={'short_url': self.tiny_link.short_url})
+        self.tiny_link_statistics = reverse("tinylink_statistics")
+        self.tiny_link_db_stats = reverse("api_db_stats")
+        self.tiny_link_api_stats = reverse("api_db_stats")
+        self.tiny_link_url_stats = reverse(
+            "api_url_stats", kwargs={"short_url": self.tiny_link.short_url}
+        )
+        self.tiny_link_expand_stats = reverse(
+            "api_tinylink_expand", kwargs={"short_url": self.tiny_link.short_url}
+        )
 
     def test_api_urls(self):
         """
@@ -46,15 +60,26 @@ class TinyLinkTest(APITestCase):
         self.client.force_authenticate(user=self.user)
         tiny_link_list_response = self.client.get(path=self.tiny_link_list_url)
         tiny_link_create_response = self.client.post(path=self.tiny_link_create_url)
-        tiny_link_update_long_response = self.client.post(path=self.tiny_link_update_long_url,
-                                                          data={'long_url': 'http://www.example.com/thisisalongURLCustom'})
-        tiny_link_update_short_response = self.client.post(path=self.tiny_link_update_short_url,
-                                                           data={'long_url': 'http://www.example.com/thisisalongURLCustom', 'short_url': 'aaq12'})
-        tiny_link_statistics_response = self.client.get(path=self.tiny_link_statistics, data={'testing': True})
+        tiny_link_update_long_response = self.client.post(
+            path=self.tiny_link_update_long_url,
+            data={"long_url": "http://www.example.com/thisisalongURLCustom"},
+        )
+        tiny_link_update_short_response = self.client.post(
+            path=self.tiny_link_update_short_url,
+            data={
+                "long_url": "http://www.example.com/thisisalongURLCustom",
+                "short_url": "aaq12",
+            },
+        )
+        tiny_link_statistics_response = self.client.get(
+            path=self.tiny_link_statistics, data={"testing": True}
+        )
         tiny_link_db_stats_response = self.client.get(path=self.tiny_link_db_stats)
         tiny_link_api_stats_response = self.client.get(path=self.tiny_link_api_stats)
         tiny_link_url_stats_response = self.client.get(path=self.tiny_link_url_stats)
-        tiny_link_expand_stats_response = self.client.get(path=self.tiny_link_expand_stats)
+        tiny_link_expand_stats_response = self.client.get(
+            path=self.tiny_link_expand_stats
+        )
 
         print("Testing:\nTiny Link List")
         self.assertEqual(tiny_link_list_response.status_code, status.HTTP_302_FOUND)
@@ -65,11 +90,15 @@ class TinyLinkTest(APITestCase):
         print("Testing: Completed")
 
         print("\nTesting:\nTiny Link Updated For Long URL")
-        self.assertEqual(tiny_link_update_long_response.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(
+            tiny_link_update_long_response.status_code, status.HTTP_302_FOUND
+        )
         print("Testing: Completed")
 
         print("\nTesting:\nTiny Link Updated For Short URL")
-        self.assertEqual(tiny_link_update_short_response.status_code, status.HTTP_302_FOUND)
+        self.assertEqual(
+            tiny_link_update_short_response.status_code, status.HTTP_302_FOUND
+        )
         print("Testing: Completed")
 
         print("\nTesting:\nTiny Link Statistics")
@@ -89,5 +118,49 @@ class TinyLinkTest(APITestCase):
         print("Testing: Completed")
 
         print("\nTesting:\nTiny Link API Expand")
-        self.assertEqual(tiny_link_expand_stats_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            tiny_link_expand_stats_response.status_code, status.HTTP_200_OK
+        )
         print("Testing: Completed")
+
+
+class TinylinkRestViewTest(APITestCase):
+    def setUp(self):
+        super(TinylinkViewTest, self).setUp()
+        self.user = User.objects.create_superuser(
+            username="gerges",
+            email="gerges_test@kuwaitnet.com",
+            password=make_password("password123"),
+        )
+
+    def test_authenticated_admin_links_list(self):
+        """
+        test get links list if user.is_admin = True
+        """
+        self.client.force_authenticate(user=self.user)
+        tiny_link_list_response = self.client.get("/s/api/tinylinks/")
+        print(
+            "\nTesting:\nTiny Link List Links : user.is_admin=True & user is authenticated"
+        )
+        self.assertEqual(tiny_link_list_response.status_code, status.HTTP_200_OK)
+
+    def test_unauthenticated_admin_links_list(self):
+        """
+        test get links list if user.is_admin = False
+        """
+        tiny_link_list_response = self.client.get("/s/api/tinylinks/")
+        print(
+            "\nTesting:\nTiny Link List Links : user.is_admin=False & user in not authenticated"
+        )
+        self.assertEqual(
+            tiny_link_list_response.status_code, status.HTTP_401_UNAUTHORIZED
+        )
+
+    def test_authenticated_admin_create_link(self):
+        self.query_parameter = "https://soundcloud.com/discover"
+        self.client.force_authenticate(user=self.user)
+        tiny_link_create_response = self.client.post(
+            "/s/api/tinylinks/", {"long_url": self.query_parameter}, format="json"
+        )
+        print("\nTesting:\nTiny Link create One Link")
+        self.assertEqual(tiny_link_create_response.status_code, status.HTTP_201_CREATED)
--- tinylinks/views.py
+++ tinylinks/views.py
@@ -24,11 +24,15 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from tinylinks.serializers import TinylinkSerializer, UserSerializer
 
+from django.shortcuts import get_list_or_404
+from django.db.models import Q
+from django.utils.crypto import get_random_string
+
 import re
 
 User = get_user_model()
 
-piwik_id = re.compile(r'^_pk_id')
+piwik_id = re.compile(r"^_pk_id")
 
 
 class TinylinkViewMixin(object):
@@ -36,39 +40,42 @@ class TinylinkViewMixin(object):
     View to handle general functions for Tinylink objects.
 
     """
+
     model = Tinylink
     form_class = TinylinkForm
 
-    @method_decorator(permission_required('tinylinks.add_tinylink'))
+    @method_decorator(permission_required("tinylinks.add_tinylink"))
     def dispatch(self, *args, **kwargs):
-        self.mode = kwargs.get('mode')
+        self.mode = kwargs.get("mode")
         return super(TinylinkViewMixin, self).dispatch(*args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(TinylinkViewMixin, self).get_context_data(**kwargs)
-        context.update({'mode': self.mode})
+        context.update({"mode": self.mode})
         return context
 
     def get(self, request, *args, **kwargs):
         self.object = None
-        if hasattr(self, 'get_object') and kwargs.get('pk'):
+        if hasattr(self, "get_object") and kwargs.get("pk"):
             self.object = self.get_object()
-            if (not request.user.is_staff
-                and (not self.object
-                     or not self.object.user == request.user)):
+            if not request.user.is_staff and (
+                not self.object or not self.object.user == request.user
+            ):
                 raise Http404
         return super(TinylinkViewMixin, self).get(request, *args, **kwargs)
 
     def get_form_kwargs(self):
         kwargs = super(TinylinkViewMixin, self).get_form_kwargs()
-        kwargs.update({
-            'user': self.request.user,
-            'mode': self.mode,
-        })
+        kwargs.update(
+            {
+                "user": self.request.user,
+                "mode": self.mode,
+            }
+        )
         return kwargs
 
     def get_success_url(self):
-        return reverse('tinylink_list')
+        return reverse("tinylink_list")
 
 
 class TinylinkListView(TinylinkViewMixin, ListView):
@@ -76,15 +83,16 @@ class TinylinkListView(TinylinkViewMixin, ListView):
     View to list all tinylinks of a user.
 
     """
+
     queryset = Tinylink.objects.all()
 
-    @method_decorator(permission_required('tinylinks.add_tinylink'))
+    @method_decorator(permission_required("tinylinks.add_tinylink"))
     def dispatch(self, request, *args, **kwargs):
-        if request.method == 'POST':
+        if request.method == "POST":
             for key in request.POST:
-                if key.startswith('validate'):
+                if key.startswith("validate"):
                     try:
-                        link_id = int(key.replace('validate', ''))
+                        link_id = int(key.replace("validate", ""))
                     except ValueError:
                         raise Http404
                     try:
@@ -107,8 +115,9 @@ class TinylinkCreateView(TinylinkViewMixin, CreateView):
     """
 
     def get_success_url(self):
-        return reverse('tinylink_update', kwargs={'pk': self.object.id,
-                                                  'mode': 'change-short'})
+        return reverse(
+            "tinylink_update", kwargs={"pk": self.object.id, "mode": "change-short"}
+        )
 
 
 class TinylinkUpdateView(TinylinkViewMixin, UpdateView):
@@ -116,6 +125,7 @@ class TinylinkUpdateView(TinylinkViewMixin, UpdateView):
     View to update a Tinylink instance.
 
     """
+
     pass
 
 
@@ -124,6 +134,7 @@ class TinylinkDeleteView(TinylinkViewMixin, DeleteView):
     View to delete a certain tinylink.
 
     """
+
     pass
 
 
@@ -134,13 +145,12 @@ class TinylinkRedirectView(RedirectView):
     """
 
     def dispatch(self, *args, **kwargs):
-        if kwargs.get('short_url'):
+        if kwargs.get("short_url"):
             try:
-                tinylink = Tinylink.objects.get(short_url=kwargs.get(
-                    'short_url'))
+                tinylink = Tinylink.objects.get(short_url=kwargs.get("short_url"))
             except Tinylink.DoesNotExist:
                 tinylink = None
-                self.url = reverse('tinylink_notfound')
+                self.url = reverse("tinylink_notfound")
             if tinylink:
                 # set the redirect long URL
                 self.url = tinylink.long_url
@@ -148,20 +158,22 @@ class TinylinkRedirectView(RedirectView):
                 tinylink.save()
 
                 try:
-                    ref = self.request.META.get('HTTP_REFERER', '')
+                    ref = self.request.META.get("HTTP_REFERER", "")
                 except KeyError:
-                    ref = ''
+                    ref = ""
 
                 cookies = self.request.COOKIES
-                pk_id = ''
+                pk_id = ""
                 for key in cookies:
                     if piwik_id.search(key):
                         pk_id = cookies[key]
 
                 tlog = TinylinkLog(
-                    tinylink=tinylink, referrer=ref, cookie=pk_id,
-                    user_agent=self.request.META.get('HTTP_USER_AGENT', ''),
-                    remote_ip=self.request.META['REMOTE_ADDR']
+                    tinylink=tinylink,
+                    referrer=ref,
+                    cookie=pk_id,
+                    user_agent=self.request.META.get("HTTP_USER_AGENT", ""),
+                    remote_ip=self.request.META["REMOTE_ADDR"],
                 )
                 tlog.save()
 
@@ -177,7 +189,7 @@ class TinylinkRedirectView(RedirectView):
         """
         if self.url:
             url = self.url
-            args = self.request.META.get('QUERY_STRING', '')
+            args = self.request.META.get("QUERY_STRING", "")
             if args and self.query_string:
                 url = "%s?%s" % (url, args)
             return url
@@ -190,11 +202,12 @@ class StatisticsView(ListView):
     View to list all tinylinks including their statistics.
 
     """
+
     model = Tinylink
     template_name = "tinylinks/statistics.html"
 
     def dispatch(self, request, *args, **kwargs):
-        if not (request.user.is_staff or request.GET.get('testing')):
+        if not (request.user.is_staff or request.GET.get("testing")):
             raise Http404
         return super(StatisticsView, self).dispatch(request, *args, **kwargs)
 
@@ -204,13 +217,46 @@ class TinylinkViewSet(viewsets.ModelViewSet):
     This viewset provides `list`, `create`, `retrieve`, `update` and `destroy` actions.
 
     """
+
     queryset = Tinylink.objects.all()
     serializer_class = TinylinkSerializer
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    authentication_classes = (
+        SessionAuthentication,
+        BasicAuthentication,
+    )
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def pre_save(self, obj):
         obj.user = self.request.user
+
+    def get_queryset(self):
+        request_url = self.request.GET.get("url", None)
+        if not request_url and self.request.user.is_staff:
+            return Tinylink.objects.all()
+        elif not request_url and not self.request.user.is_staff:
+            return Tinylink.objects.filter(user=self.request.user)
+
+        query_data = get_list_or_404(
+            Tinylink, Q(short_url=request_url) | Q(long_url=request_url)
+        )
+        return query_data
+
+    def create(self, request, *args, **kwargs):
+        request.data["short_url"] = get_random_string(6)
+        request.data["user"] = request.user.id
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        data = {
+            "id":serializer.data['id'],
+            "short_url":request.build_absolute_uri('/s/%s'%serializer.data['short_url']),
+            "long_url":serializer.data['long_url']
+            }
+
+        return Response(
+            data, status=status.HTTP_201_CREATED, headers=headers
+        )
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
@@ -218,6 +264,7 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     This viewset automatically provides `list` and `detail` actions.
 
     """
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
@@ -228,13 +275,14 @@ def database_statistics():
     all clicks the system recorded
     """
     return {
-        'tinylinks': Tinylink.objects.count(),
-        'clicks': Tinylink.objects.aggregate(Sum('amount_of_views'))
-            .get('amount_of_views__sum', 0)
+        "tinylinks": Tinylink.objects.count(),
+        "clicks": Tinylink.objects.aggregate(Sum("amount_of_views")).get(
+            "amount_of_views__sum", 0
+        ),
     }
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def db_stats(request):
     """
     Total number of tinylinks and sum of clicks
@@ -245,7 +293,7 @@ def db_stats(request):
     return Response(data)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def stats(request):
     """
     Stats about tinylinks
@@ -253,8 +301,8 @@ def stats(request):
     """
 
     try:
-        paginate_by = int(request.QUERY_PARAMS.get('paginate_by', ''))
-        page = int(request.QUERY_PARAMS.get('page', ''))
+        paginate_by = int(request.QUERY_PARAMS.get("paginate_by", ""))
+        page = int(request.QUERY_PARAMS.get("page", ""))
     except:
         paginate_by = 10
         page = 1
@@ -262,11 +310,11 @@ def stats(request):
     links = {}
     count = 0
     for link in Tinylink.objects.all():
-        links['link_' + str(count)] = {
-            'shorturl': link.short_url,
-            'url': link.long_url,
-            'clicks': link.amount_of_views,
-            'is_broken': link.is_broken
+        links["link_" + str(count)] = {
+            "shorturl": link.short_url,
+            "url": link.long_url,
+            "clicks": link.amount_of_views,
+            "is_broken": link.is_broken,
         }
         count += 1
 
@@ -281,13 +329,13 @@ def stats(request):
         links = paginator.page(paginator.num_pages)
 
     data = {}
-    data['links'] = links.object_list
-    data['stats'] = database_statistics()
+    data["links"] = links.object_list
+    data["stats"] = database_statistics()
 
     return Response(data)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def tinylink_stats(request, short_url):
     """
     Return stats for a link
@@ -297,20 +345,20 @@ def tinylink_stats(request, short_url):
     tinylink = Tinylink.objects.get(short_url=short_url)
 
     if not tinylink:
-        data = {'message': 'Error: Link not found'}
+        data = {"message": "Error: Link not found"}
         return Response(data, status=status.HTTP_404_NOT_FOUND)
 
     data = {}
-    data['link'] = {
-        'short_url': tinylink.short_url,
-        'long_url': tinylink.long_url,
-        'clicks': tinylink.amount_of_views
+    data["link"] = {
+        "short_url": tinylink.short_url,
+        "long_url": tinylink.long_url,
+        "clicks": tinylink.amount_of_views,
     }
 
     return Response(data)
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def tinylink_expand(request, short_url):
     """
     Expand a short URL into a long URL
@@ -320,12 +368,12 @@ def tinylink_expand(request, short_url):
     tinylink = Tinylink.objects.filter(short_url=short_url)
 
     if not tinylink:
-        data = {'message': 'Error: Link not found'}
+        data = {"message": "Error: Link not found"}
         return Response(data, status=status.HTTP_404_NOT_FOUND)
 
     data = {
-        'short_url': tinylink.first().short_url,
-        'long_url': tinylink.first().long_url,
+        "short_url": tinylink.first().short_url,
+        "long_url": tinylink.first().long_url,
     }
 
     return Response(data)
