52c96218eb28f8d8a3a42466de4a628796fec84f
asmenon <adityasureshmenon@gmail.com>
2021-10-06 18:35:16 +0530

    Fixed missing pagination div. Added setting TINYLINK_PAGINATE_BY.

--- tinylinks/tests/settings.py
+++ tinylinks/tests/settings.py
@@ -13,14 +13,19 @@ import os
 
 from tinylinks.tests.test_settings import *  # NOQA
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "db.sqlite",}}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "db.sqlite",
+    }
+}
 
 SECRET_KEY = "ts4u!_y8xp!yvyxtvmk3046fxaoz_ubh4n2&qtsm&*z(-d(b07"
 
 PIWIK_ID = 1
 PIWIK_URL = "http://127.0.0.1/piwik/piwik.php"
 PIWIK_TOKEN = "60a437e4e088521ca506fcfcf4f57522"
-
+TINYLINK_PAGINATE_BY = 2
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 GEOIP_PATH = os.path.join(BASE_DIR, "geoip")
