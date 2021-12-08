628dc8727b9af4bb6569018a0091621749e57c83
asdmenon <aditya.menon@kuwaitnet.dev>
2021-12-08 16:13:57 +0530

    Coverage badge initial

---
+++ coveragebadgegen.py
@@ -0,0 +1,18 @@
+import json
+import urllib3, shutil
+try:
+    coverage = json.load(open("coverage.json"))
+    coverage_percent = coverage['totals']['percent_covered_display'] + "%"
+    try:
+        url = "https://img.shields.io/badge/Coverage-" + str(coverage_percent) + "-green"
+        http = urllib3.PoolManager()
+        filename = "coverage_badge.svg"
+        with open(filename, 'wb') as out:
+            r = http.request('GET', url, preload_content=False)
+            shutil.copyfileobj(r, out)
+        
+    except:
+        print("Failed to retrieve badge.")
+
+except:
+    print("Failed to find coverage")
