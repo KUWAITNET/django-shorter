58eccbeb6b84bede680808ae935ccf29a098b7a7
asdmenon <aditya.menon@kuwaitnet.dev>
2021-12-09 03:00:35 +0530

    Test autogen coverage

--- badgegen.py
+++ badgegen.py
@@ -1,9 +1,9 @@
 import json
-import urllib3, shutil
+import urllib3, shutil, random
 
 try:
     coverage = json.load(open('coverage.json'))
-    coverage_percent = coverage['totals']['percent_covered_display'] + "%"
+    coverage_percent = random.randint(0,100) + "%"
     try:
         url_to_dl = "https://img.shields.io/badge/Coverage-" + coverage_percent + "-green"
         
