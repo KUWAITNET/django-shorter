6e6f8868647d04fc01fbeb770d4e49964e90bb37
asdmenon <aditya.menon@kuwaitnet.dev>
2021-12-09 03:02:09 +0530

    Test gen

--- badgegen.py
+++ badgegen.py
@@ -1,9 +1,9 @@
 import json
-import urllib3, shutil, random
+import urllib3, shutil
 
 try:
     coverage = json.load(open('coverage.json'))
-    coverage_percent = random.randint(0,100) + "%"
+    coverage_percent = coverage['totals']['percent_covered_display'] + "%"
     try:
         url_to_dl = "https://img.shields.io/badge/Coverage-" + coverage_percent + "-green"
         
