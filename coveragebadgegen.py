672160e324b2293c34878b9520a57bfdcf4f7952
asdmenon <aditya.menon@kuwaitnet.dev>
2021-12-08 16:53:23 +0530

    Test coverage badge format change

--- coveragebadgegen.py
+++ coveragebadgegen.py
@@ -2,7 +2,7 @@ import json
 import urllib3, shutil
 try:
     coverage = json.load(open("coverage.json"))
-    coverage_percent = coverage['totals']['percent_covered_display'] + "%"
+    coverage_percent = coverage['totals']['percent_covered_display'] + " \% only"
     try:
         url = "https://img.shields.io/badge/Coverage-" + str(coverage_percent) + "-green"
         http = urllib3.PoolManager()
