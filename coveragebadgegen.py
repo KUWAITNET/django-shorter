733535cedb369521b4df8b46f57994d9eac48c15
asdmenon <aditya.menon@kuwaitnet.dev>
2021-12-08 17:11:54 +0530

    Test coverage 2

--- coveragebadgegen.py
+++ coveragebadgegen.py
@@ -2,7 +2,7 @@ import json
 import urllib3, shutil
 try:
     coverage = json.load(open("coverage.json"))
-    coverage_percent = coverage['totals']['percent_covered_display'] + " \% only"
+    coverage_percent = coverage['totals']['percent_covered_display'] + "%"
     try:
         url = "https://img.shields.io/badge/Coverage-" + str(coverage_percent) + "-green"        
         fname = open('test.txt', 'w')
